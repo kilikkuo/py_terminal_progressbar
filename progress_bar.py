c17875fc3f461fab57eebc32b288893dc7cedf77
Kilik Kuo <kikuo@mozilla.com>
2018-02-04 15:59:00 +0800

    Add more description

--- progress_bar.py
+++ progress_bar.py
@@ -1,18 +1,22 @@
 import sys
 def display_progress(idx, total):
-    # idx : from 0 to total - 1
-    # total : The number of proceeding tasks.
+    # 此函數的設計是要在迴圈內被重複呼叫 !!
+    # idx : 進行任務的索引, 從 0 ~ total -1.
+    # total : 要進行的任務次數
 
-    # How many "=" will be displayed when 100 % is achieved.
+    # 當螢幕顯示 50 個 "=", 進度即抵達 100 %
     num_of_bar = 50
 
-    # Should be inside a loop, total should be fixed ! idx starts from 0
+    # 計算一個進度單位
     unit = total / 100.0
     fprogress = " %3.2f"%((idx+1) / unit)
 
+    # 計算有多少百分比已經完成
     done = int((idx+1) * num_of_bar / total)
+    # 計算有多少百分比尚未未成
     not_yet = num_of_bar - done
 
+    # 透過字串組合將結果連結起來
     sys.stdout.write("\r[" + "="*done +  " "*not_yet + "]" + fprogress + "%")
     sys.stdout.flush()
 
